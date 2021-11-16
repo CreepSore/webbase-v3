387a68dbbbe05b6665936a9433fc579f7613666d
Eric <eric.himmelbauer@protonmail.com>
2021-11-16 14:59:42 +0100

    added cache provider

---
+++ src/service/CacheProvider.js
@@ -0,0 +1,43 @@
+
+export default class CacheProvider {
+    static #instance;
+    static get instance() {
+        return this.#instance || (this.#instance = new CacheProvider());
+    }
+
+    constructor() {
+        this.cache = {};
+    }
+
+    unregister(name) {
+        delete this.cache[name];
+        return this;
+    }
+
+    /**
+     * @param {string} name
+     * @param {Function} callback
+     * @param {number} ttlMs
+     * @return {Promise<any>}
+     * @memberof CacheProvider
+     */
+    async process(name, callback, ttlMs) {
+        let cacheObject = this.cache[name];
+        if(cacheObject) {
+            if(Date.now() - cacheObject.lastFetch < ttlMs) return cacheObject.data;
+            cacheObject.data = await cacheObject.callback();
+            cacheObject.lastFetch = Date.now();
+            return cacheObject.data;
+        }
+
+        cacheObject = this.cache[name] = {
+            name,
+            callback,
+            ttlMs,
+            lastFetch: Date.now(),
+            data: await callback()
+        };
+
+        return cacheObject.data;
+    }
+}
