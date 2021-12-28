99b018148916b8f0dbd6ab0d88a3e27f1cfc7a76
Eric <eric.himmelbauer@protonmail.com>
2021-12-28 21:39:51 +0100

    added tailwind css nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,10 @@
 let tailwindcss = require("tailwindcss");
+let tailwindNesting = require("tailwindcss/nesting");
 
 module.exports = {
     plugins: [
         "postcss-preset-env",
+        tailwindNesting,
         tailwindcss
     ]
 };
