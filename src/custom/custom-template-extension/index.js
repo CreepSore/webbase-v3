e0ae0dc6bc369f3da25ccb3e4cd5a1af2d8fa61a
Eric <eric.himmelbauer@protonmail.com>
2022-01-10 14:49:18 +0100

    added example extension

---
+++ src/custom/custom-template-extension/index.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars */
+import CustomerLogic from "../../service/customer-logic/CustomerLogic.js";
+import Version from "../../models/Version.js";
+
+/**
+ * @typedef {import("../../service/customer-logic/types").CustomerLogicDependencies} CustomerLogicDependencies
+ * @typedef {import("../../service/customer-logic/types").SequelizeParams} SequelizeParams
+ * @typedef {import("../../service/customer-logic/types").ExpressParams} ExpressParams
+ * @typedef {import("../../service/customer-logic/types").StartCliApplicationParams} StartCliApplicationParams
+ */
+
+export default class Core extends CustomerLogic {
+    /** @param {StartCliApplicationParams} params */
+    async onStartCliApplication(params) {}
+
+    async onStartInstallerApplication() {}
+
+    async onStartMainApplication() {}
+
+    /** @param {SequelizeParams} params */
+    async sequelizeSetupModels(params) {}
+
+    /** @param {SequelizeParams} params */
+    async sequelizeFirstInstall(params) {
+        await Version.create({
+            name: this.getMetadata().name,
+            version: this.getMetadata().version
+        });
+    }
+
+    /** @param {SequelizeParams} params */
+    async sequelizeSetupRelation(params) {}
+
+    /** @param {ExpressParams} params */
+    async expressStart(params) { }
+
+    /** @param {ExpressParams} params */
+    async expressStop(params) {}
+
+    getWebpackConfig(params) {
+        return {};
+    }
+
+    getPriority() {return 1000;}
+    async onLoad() { }
+    async onUnload() {}
+}
