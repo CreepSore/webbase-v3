9156d379379d293bc1625a90e2e3852b73e32c4a
Eric <eric.himmelbauer@protonmail.com>
2022-01-24 12:11:47 +0100

    reworked profiling

--- src/custom/core-profiling/web/src/Profiling.jsx
+++ src/custom/core-profiling/web/src/Profiling.jsx
@@ -3,105 +3,70 @@ import * as IconsSolid from "@heroicons/react/solid";
 import Editor from "@monaco-editor/react";
 
 import ProfilingApi from "./ProfilingApi.js";
+import SelectorList from "./SelectorList.jsx";
 import "./profiling.css";
 
-export default class Profiling extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: {},
-            keys: [],
-            selectedEntry: null,
-            measurements: null,
-            measurement: null
-        };
-        this.fetchData();
-    }
+export default function Profiling() {
+    const [profilingData, setData] = React.useState([]);
+    const [entries, setEntries] = React.useState([]);
+    const [selectedEntry, setSelectedEntry] = React.useState(null);
 
-    render = () => {
-        return (
-            <div className="profiling">
-                <div className="profiling-bar">
-                    <div className="logo">
-                        <span>
-                            <IconsSolid.ChartBarIcon height={32} width={32} className="text-white ml-2 inline" />
-                            <h1 className="inline">Profiling</h1>
-                        </span>
-                    </div>
+    React.useEffect(() => {
+        (async() => {
+            let isEnabled = await ProfilingApi.getIsEnabled();
+            if(!isEnabled) {
+                location.href = "/";
+            }
+
+            let newData = await ProfilingApi.fetchProfilingData();
+            let convertedData = Object.entries(newData)
+                .map(([key, value]) => {
+                    return {
+                        key,
+                        data: value.filter(entry => Boolean(entry.endTime))
+                    };
+                }).filter(entry => entry.data.length > 0)
+                .sort((a, b) => a.key.localeCompare(b.key));
+            setData(convertedData);
+        })();
+    }, []);
+
+    return (
+        <div className="profiling">
+            <div className="profiling-bar">
+                <div className="logo">
+                    <span>
+                        <IconsSolid.ChartBarIcon height={32} width={32} className="text-white ml-2 inline" />
+                        <h1 className="inline">Profiling</h1>
+                    </span>
                 </div>
-                <div className="profiling-container">
-                    <div className="profiling-container-col">
-                        <div className="profiling-table-container">
-                            <table className="profiling-table font-[Roboto Mono]">
-                                <tbody>
-                                    {this.measurementGroups.map(group => (
-                                        <tr
-                                            key={group}
-                                            className="cursor-pointer"
-                                            onClick={() => this.setMeasurements(group)}>
-                                            <td>{group}</td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div className="profiling-container-col">
-                        <div className="profiling-table-container">
-                            <table className="profiling-table font-[Roboto Mono]">
-                                <tbody>
-                                    {(this.state.measurements || []).map(measurement => {
-                                        return (
-                                            <tr
-                                                key={measurement.id}
-                                                className="cursor-pointer"
-                                                onClick={() => this.setMeasurement(measurement)}>
-                                                <td>{measurement.duration} ms</td>
-                                            </tr>
-                                        );
-                                    })}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    <div className="profiling-container-col">
-                        <div hidden={!Boolean(this.state.measurement?.additionalData)}>
-                            <Editor
-                                height={`${this.monacoText.split("\n").length + 10}rem`}
-                                value={this.monacoText}
-                                options={{readOnly: true}}
-                                language="json"/>
-                        </div>
+            </div>
+
+            <div className="profiling-container">
+                <div className="profiling-container-col">
+                    <SelectorList
+                        arrowType={"right"}
+                        onSelectionChanged={(key) => {setEntries(profilingData.find(entry => entry.key === key).data);}}
+                        entries={profilingData.map(d => {return {key: d.key, label: d.key};})}></SelectorList>
+                </div>
+
+                <div className="profiling-container-col">
+                    <SelectorList
+                        arrowType={"right"}
+                        onSelectionChanged={(key) => setSelectedEntry(entries.find(e => e.id === key))}
+                        entries={entries.map(d => {return {key: d.id, label: `${d.duration} ms`};})}></SelectorList>
+                </div>
+
+                <div className="profiling-container-col">
+                    <div className="h-full" hidden={!Boolean(selectedEntry)}>
+                        <Editor
+                            height={`${JSON.stringify(selectedEntry || {}, null, 2).split("\n").length + 10}rem`}
+                            value={JSON.stringify(selectedEntry || {}, null, 2)}
+                            options={{readOnly: true}}
+                            language="json"/>
                     </div>
                 </div>
             </div>
-        );
-    }
-
-    setMeasurement = (measurement) => {
-        this.setState({measurement});
-    }
-
-    setMeasurements = (measurementKey) => {
-        this.setState({measurements: this.state.data[measurementKey], measurement: null});
-    }
-
-    get monacoText() {
-        if(!this.state.measurement) return "";
-        return JSON.stringify(this.state.measurement?.additionalData, null, 2);
-    }
-
-    get measurementGroups() {
-        return Object.keys(this.state.data || {});
-    }
-
-    fetchData = async() => {
-        let isEnabled = await ProfilingApi.getIsEnabled();
-        if(!isEnabled) {
-            location.href = "/";
-        }
-
-        let data = await ProfilingApi.fetchProfilingData();
-        this.setState({data, keys: Object.entries(data).map(([k, v]) => {return {name: k, count: v.length};})});
-    }
+        </div>
+    );
 }
