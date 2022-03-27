427d922a9ff492f5896f407c5edba7f7840e5ddf
CreepSore <eric.himmelbauer@protonmail.com>
2022-03-27 12:44:44 +0200

    removed express session sequelize store

--- src/custom/core/index.js
+++ src/custom/core/index.js
@@ -7,7 +7,6 @@ import expressWs from "express-ws";
 import * as uuid from "uuid";
 import helmet from "helmet";
 import expressSession from "express-session";
-import expressSessionSequelize from "express-session-sequelize";
 
 import Setting from "./models/Setting.js";
 
@@ -111,11 +110,6 @@ export default class Core extends CustomerLogic {
 
     /** @param {SequelizeParams} params */
     async sequelizeSetupModels(params) {
-        let SessionStore = expressSessionSequelize(expressSession.Store);
-        this.sessionStore = new SessionStore({
-            db: params.sequelize
-        });
-
         Setting.initialize(params.sequelize);
     }
 
@@ -150,8 +144,6 @@ export default class Core extends CustomerLogic {
             secret: cfg.web.sessionSecret || uuid.v4(),
             saveUninitialized: false,
             resave: false,
-            // @ts-ignore
-            store: this.sessionStore,
             cookie: {
                 maxAge: 60000 * 60
             }
